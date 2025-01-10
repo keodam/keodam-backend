910bb70d281484f5e45c18e0bbbe34267d9ba950
seunghyun0506 <jno444@kumoh.ac.kr>
2025-01-10 16:02:04 +0900

    create Spring Boot Project

---
+++ src/main/java/com/keodam/keodam_backend/KeodamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.keodam.keodam_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeodamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeodamBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keodam/keodam_backend/KeodamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keodam.keodam_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeodamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
