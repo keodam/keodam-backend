2dac3999e8f03f05c6f726e52327c67e598c9e69
seunghyun0506 <jno444@kumoh.ac.kr>
2025-01-13 03:35:03 +0900

    add : 로그인 구현

--- src/main/java/com/keodam/keodam_backend/global/config/SecurityConfig.java
+++ src/main/java/com/keodam/keodam_backend/global/config/SecurityConfig.java
@@ -1,27 +1,20 @@
 package com.keodam.keodam_backend.global.config;
 
 import com.keodam.keodam_backend.app.repository.UserRepository;
-import com.keodam.keodam_backend.global.security.IdTokenAuthenticationFilter;
 import com.keodam.keodam_backend.global.security.JwtAuthenticationProcessingFilter;
 import com.keodam.keodam_backend.global.security.JwtService;
 import com.keodam.keodam_backend.oauth.domain.CustomIdTokenUser;
 import com.keodam.keodam_backend.oauth.service.IdTokenService;
 import com.keodam.keodam_backend.oauth.service.handler.IdTokenLoginSuccessHandler;
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
