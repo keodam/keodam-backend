b906e4a57ee8d46525753dbac8546eb7718783af
seunghyun0506 <jno444@kumoh.ac.kr>
2025-01-14 04:05:39 +0900

    add : 스웨거 추가

--- src/main/java/com/keodam/keodam_backend/oauth/controller/AuthController.java
+++ src/main/java/com/keodam/keodam_backend/oauth/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.keodam.keodam_backend.oauth.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.headers.Header;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +16,12 @@ public class AuthController {
 
     @GetMapping("/auth/login")
     @Operation(summary = "소셜 로그인", description = "OIDC 전송하여 로그인하는 소셜 로그인 API")
-
+    @Parameter(
+            name = "id_token",
+            description = "Bearer 토큰을 제외한 OIDC Token 헤더",
+            required = true,
+            in = ParameterIn.HEADER
+    )
     public String login() {
 
         return "필터에서 처리";
