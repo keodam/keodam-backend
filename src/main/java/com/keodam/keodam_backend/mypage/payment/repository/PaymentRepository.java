2846ca2a427f12395f8edc61447d61906e4cecb5
jihoo2002 <yolohoo@naver.com>
2025-04-15 15:11:40 +0900

    Feat(#16) : PaymentRepository 클래스 생성

---
+++ src/main/java/com/keodam/keodam_backend/mypage/payment/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package com.keodam.keodam_backend.mypage.payment.repository;
+
+import com.keodam.keodam_backend.mypage.payment.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, String> {
+}
