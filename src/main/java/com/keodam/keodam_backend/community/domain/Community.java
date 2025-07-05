c0047ab3d5d0ba1ecd8900c262988d553dd91474
jihoo2002 <yolohoo@naver.com>
2025-07-05 14:21:32 +0900

    ADD(#36) : 커뮤니티 도메인 클래스 builder, 해시태그 메서드 추가

--- src/main/java/com/keodam/keodam_backend/community/domain/Community.java
+++ src/main/java/com/keodam/keodam_backend/community/domain/Community.java
@@ -1,8 +1,13 @@
 package com.keodam.keodam_backend.community.domain;
 
+import com.keodam.keodam_backend.app.domain.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,4 +25,23 @@ public class Community {
 
     @OneToMany(mappedBy = "community", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CommunityHashTag> communityHashTags = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreatedDate
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Builder
+    public Community(String content, User user) {
+        this.content = content;
+        this.creationDate = LocalDateTime.now();
+        this.user = user;
+    }
+
+    public void addCommunityHashTag(CommunityHashTag communityHashTag) {
+        this.communityHashTags.add(communityHashTag);
+    }
 }
