7f74ec76d53a52c371f8c9a4f2f9d88691549e17
jihoo2002 <yolohoo@naver.com>
2025-07-05 14:23:21 +0900

    ADD(#36) : CommunityRequestDto 클래스 추가

---
+++ src/main/java/com/keodam/keodam_backend/community/dto/request/CommunityRequestDto.java
@@ -0,0 +1,11 @@
+package com.keodam.keodam_backend.community.dto.request;
+
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+public class CommunityRequestDto {
+
+    private String content;
+    private List<String> tags;
+}
