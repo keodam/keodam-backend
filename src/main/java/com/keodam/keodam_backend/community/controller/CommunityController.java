ec44a69451ba009c33942350934d315782175919
jihoo2002 <yolohoo@naver.com>
2025-07-05 19:52:51 +0900

    Feat(#36) : 커뮤니티 게시물 조회 메서드 추가

--- src/main/java/com/keodam/keodam_backend/community/controller/CommunityController.java
+++ src/main/java/com/keodam/keodam_backend/community/controller/CommunityController.java
@@ -1,5 +1,6 @@
 package com.keodam.keodam_backend.community.controller;
 
+import com.keodam.keodam_backend.community.domain.Community;
 import com.keodam.keodam_backend.community.dto.request.CommunityRequestDto;
 import com.keodam.keodam_backend.community.dto.response.CommunityResponseDto;
 import com.keodam.keodam_backend.community.service.CommunityService;
@@ -8,10 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -26,13 +24,18 @@ public class CommunityController {
     @PostMapping(value = "/create", consumes = "multipart/form-data")
     @Operation(summary = "커뮤니티 게시물 작성", description = "커뮤니티 게시물 작성 API", security = @SecurityRequirement(name = "Authorization"))
     public ApiResponse<CommunityResponseDto> createCommunityPost(Authentication authentication,
-                                                      @RequestPart CommunityRequestDto communityRequestDto,
-                                                      @RequestPart(name = "ImageFile", required = false) List<MultipartFile> imgs
+                                                                 @RequestPart CommunityRequestDto communityRequestDto,
+                                                                 @RequestPart(name = "ImageFile", required = false) List<MultipartFile> imgs
     ) {
         String email = authentication.getName();
         return ApiResponse.onSuccess(communityService.createCommunityPost(email, communityRequestDto, imgs));
     }
 
+    @PostMapping("/{communityId}")
+    public ApiResponse<CommunityResponseDto> getCommunityPost(@PathVariable Long communityId) {
+        return ApiResponse.onSuccess(communityService.getCommunityPost(communityId));
+
+    }
 
 
 }
