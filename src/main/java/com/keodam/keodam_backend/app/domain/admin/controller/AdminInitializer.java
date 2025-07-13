fab7a57bb28c38c9612ea35147a5b7f1e020efc0
윤준석(YunJunSeok) <83647215+junseok0304@users.noreply.github.com>
2025-07-13 21:53:59 +0900

    Feat/#13: FE-WEB에서 코드 테스트할 기간동안 사용할 유령계정 생성기를 만들었습니다. 실제 운영 환경에서는 삭제할 코드이며, 운영 전 DB 초기화 예정입니다.

---
+++ src/main/java/com/keodam/keodam_backend/app/domain/admin/controller/AdminInitializer.java
@@ -0,0 +1,43 @@
+package com.keodam.keodam_backend.app.domain.admin.controller;
+
+import com.keodam.keodam_backend.app.domain.admin.domain.Admin;
+import com.keodam.keodam_backend.app.domain.admin.domain.RoleTypeAdmin;
+import com.keodam.keodam_backend.app.domain.admin.repository.AdminRepository;
+import com.keodam.keodam_backend.exception.GeneralException;
+import com.keodam.keodam_backend.global.code.status.ErrorStatus;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AdminInitializer { // 실제 운영환경서는 삭제할 코드임.
+
+    private final AdminRepository adminRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void initSuperAdmin() {
+        String email = "superadmin@example.com";
+        String password = "SuperTemp1234";
+
+        if (adminRepository.findByEmail(email).isPresent()) {
+            return;
+        }
+
+        try {
+            Admin superAdmin = Admin.builder()
+                    .name("슈퍼관리자")
+                    .email(email)
+                    .password(passwordEncoder.encode(password))
+                    .roleType(RoleTypeAdmin.SUPER_ADMIN)
+                    .build();
+
+            adminRepository.save(superAdmin);
+
+        } catch (Exception e) {
+            throw new GeneralException(ErrorStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
