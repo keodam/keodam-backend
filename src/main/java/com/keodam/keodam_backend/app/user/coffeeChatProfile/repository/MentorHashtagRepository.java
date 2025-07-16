6db3d4b1668f9773580d2b89671686a7094c20db
jihoo2002 <yolohoo@naver.com>
2025-07-16 23:15:15 +0900

    Feat(#44) : 멘토 해시태그 레파지토리 추가

---
+++ src/main/java/com/keodam/keodam_backend/app/user/coffeeChatProfile/repository/MentorHashtagRepository.java
@@ -0,0 +1,7 @@
+package com.keodam.keodam_backend.app.user.coffeeChatProfile.repository;
+
+import com.keodam.keodam_backend.app.user.coffeeChatProfile.domain.MentorHashtag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MentorHashtagRepository extends JpaRepository<MentorHashtag, Long> {
+}
