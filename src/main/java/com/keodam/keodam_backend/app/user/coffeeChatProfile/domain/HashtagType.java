63869b22e24dd0ca6408eddefc3983f95d573581
jihoo2002 <yolohoo@naver.com>
2025-07-16 23:13:24 +0900

    Refactor(#44) : 멘토 해시태그 타입으로 네이밍 변경

--- src/main/java/com/keodam/keodam_backend/app/user/coffeeChatProfile/domain/HashtagType.java
+++
@@ -1,5 +0,0 @@
-package com.keodam.keodam_backend.app.user.coffeeChatProfile.domain;
-
-public enum HashtagType {
-    MENTEE_INTEREST
-}
---
+++ src/main/java/com/keodam/keodam_backend/app/user/coffeeChatProfile/domain/MentorHashtagType.java
@@ -0,0 +1,7 @@
+package com.keodam.keodam_backend.app.user.coffeeChatProfile.domain;
+
+public enum MentorHashtagType {
+    HELP,      // 도움 줄 수 있는 분야
+    SELF,      // 자신을 나타내는 분야
+    EVALUATED  // 평가받은 해시태그
+}
