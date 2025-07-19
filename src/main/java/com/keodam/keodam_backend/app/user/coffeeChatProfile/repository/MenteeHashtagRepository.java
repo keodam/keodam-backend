9c6e68898ed41ef00183946d2287d7a32873b581
jihoo2002 <yolohoo@naver.com>
2025-07-19 18:10:45 +0900

    Feat(#44) : deleteByMentee & findAllByMentee 메서드 추가

--- src/main/java/com/keodam/keodam_backend/app/user/coffeeChatProfile/repository/MenteeHashtagRepository.java
+++ src/main/java/com/keodam/keodam_backend/app/user/coffeeChatProfile/repository/MenteeHashtagRepository.java
@@ -1,7 +1,12 @@
 package com.keodam.keodam_backend.app.user.coffeeChatProfile.repository;
 
+import com.keodam.keodam_backend.app.user.coffeeChatProfile.domain.Mentee;
 import com.keodam.keodam_backend.app.user.coffeeChatProfile.domain.MenteeHashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MenteeHashtagRepository extends JpaRepository<MenteeHashtag, Long> {
+    void deleteByMentee(Mentee mentee);
+    List<MenteeHashtag> findAllByMentee(Mentee mentee);
 }
