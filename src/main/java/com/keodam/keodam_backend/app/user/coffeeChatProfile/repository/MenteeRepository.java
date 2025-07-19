f796a5ec4d15439740ed594800b76d4bdbc69dfd
jihoo2002 <yolohoo@naver.com>
2025-07-19 18:11:14 +0900

    Feat(#44) : 멘티 레파지토리에 findByUser 메서드 추가

--- src/main/java/com/keodam/keodam_backend/app/user/coffeeChatProfile/repository/MenteeRepository.java
+++ src/main/java/com/keodam/keodam_backend/app/user/coffeeChatProfile/repository/MenteeRepository.java
@@ -1,7 +1,11 @@
 package com.keodam.keodam_backend.app.user.coffeeChatProfile.repository;
 
+import com.keodam.keodam_backend.app.domain.User;
 import com.keodam.keodam_backend.app.user.coffeeChatProfile.domain.Mentee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MenteeRepository extends JpaRepository<Mentee, Long> {
+    Optional<Mentee> findByUser(User user);
 }
