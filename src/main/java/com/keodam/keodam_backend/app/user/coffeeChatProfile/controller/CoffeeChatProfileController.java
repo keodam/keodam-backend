80e027c6320664364b505ff3e9a404b4a6952b58
jihoo2002 <yolohoo@naver.com>
2025-07-19 18:02:13 +0900

    Feat(#44) : 멘토멘티 프로필 조회 메서드 추가

--- src/main/java/com/keodam/keodam_backend/app/user/coffeeChatProfile/controller/CoffeeChatProfileController.java
+++ src/main/java/com/keodam/keodam_backend/app/user/coffeeChatProfile/controller/CoffeeChatProfileController.java
@@ -12,10 +12,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,7 +24,7 @@ public class CoffeeChatProfileController {
     private final MentorProfileService mentorProfileService;
 
     @PatchMapping("/mentee")
-    @Operation(summary = "커피챗 멘티 프로필 등록", description = "멘티 프로필 등록 API", security = @SecurityRequirement(name = "Authorization"))
+    @Operation(summary = "커피챗 멘티 프로필 등록 및 수정", description = "멘티 프로필 등록 및 수정 API", security = @SecurityRequirement(name = "Authorization"))
     public ApiResponse<MenteeResponseDto> updateMentee(Authentication authentication,
                                                        @RequestBody MenteeRequestDto menteeRequestDto) {
         String email = authentication.getName();
@@ -35,10 +32,22 @@ public class CoffeeChatProfileController {
     }
 
     @PatchMapping("/mentor")
-    @Operation(summary = "커피챗 멘토 프로필 등록", description = "멘토 프로필 등록 API", security = @SecurityRequirement(name = "Authorization"))
+    @Operation(summary = "커피챗 멘토 프로필 등록 및 수정", description = "멘토 프로필 등록 및 수정 API", security = @SecurityRequirement(name = "Authorization"))
     public ApiResponse<MentorResponseDto> updateMentor(Authentication authentication,
                                                        @RequestBody MentorRequestDto mentorRequestDto) {
         String email = authentication.getName();
         return ApiResponse.onSuccess(mentorProfileService.updateMentor(email, mentorRequestDto));
+    }
+
+    @GetMapping("/mentee/{menteeId}")
+    @Operation(summary = "커피챗 멘티 프로필 조회", description = "멘티 프로필 조회", security = @SecurityRequirement(name = "Authorization"))
+    public ApiResponse<MenteeResponseDto> getMentee(@PathVariable Long menteeId) {
+        return ApiResponse.onSuccess( menteeProfileService.getMentee(menteeId));
+    }
+
+    @GetMapping("/mentor/{mentorId}")
+    @Operation(summary = "커피챗 멘토 프로필 조회", description = "멘토 프로필 조회", security = @SecurityRequirement(name = "Authorization"))
+    public ApiResponse<MentorResponseDto> getMentor(@PathVariable Long mentorId) {
+        return ApiResponse.onSuccess(mentorProfileService.getMentor(mentorId));
     }
 }
