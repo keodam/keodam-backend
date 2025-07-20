31008f7462a09871d14ae558a9d92b3f20bcee0f
seunghyun0506 <jno444@kumoh.ac.kr>
2025-07-21 04:09:12 +0900

    refactor: 폴더 구조 개선

--- src/main/java/com/keodam/keodam_backend/global/security/oidc/domain/userinfo/GoogleUserInfo.java
+++ src/main/java/com/keodam/keodam_backend/global/security/oidc/domain/userinfo/GoogleUserInfo.java
@@ -1,4 +1,4 @@
-package com.keodam.keodam_backend.oauth.domain.userinfo;
+package com.keodam.keodam_backend.global.security.oidc.domain.userinfo;
 
 import lombok.extern.slf4j.Slf4j;
 import java.util.Map;
