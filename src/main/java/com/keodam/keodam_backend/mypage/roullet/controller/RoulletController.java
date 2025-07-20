8482d729e6f78e409b7c017e6c9fd8064cd94476
윤준석(YunJunSeok) <83647215+junseok0304@users.noreply.github.com>
2025-07-20 20:07:14 +0900

    Feat/#11: 룰렛 관련 코드 작성중

---
+++ src/main/java/com/keodam/keodam_backend/mypage/roullet/controller/RoulletController.java
@@ -0,0 +1,4 @@
+package com.keodam.keodam_backend.mypage.roullet.controller;
+
+public class RoulletController {
+}
---
+++ src/main/java/com/keodam/keodam_backend/mypage/roullet/domain/SpinResultType.java
@@ -0,0 +1,8 @@
+package com.keodam.keodam_backend.mypage.roullet.domain;
+
+public enum SpinResultType {
+    NONE,
+    EXP150,
+    EXP300,
+    COUPON
+}
