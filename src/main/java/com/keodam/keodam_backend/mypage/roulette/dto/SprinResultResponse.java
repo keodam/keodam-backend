52b6edf9d587cdd7d00576b676eef5b2dc8014ae
윤준석(YunJunSeok) <83647215+junseok0304@users.noreply.github.com>
2025-07-29 21:55:02 +0900

    Feat/#11: 룰렛 구현 2 Dto 작성

---
+++ src/main/java/com/keodam/keodam_backend/mypage/roulette/dto/SprinResultResponse.java
@@ -0,0 +1,11 @@
+package com.keodam.keodam_backend.mypage.roulette.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SprinResultResponse {
+    private String result;
+    private int remainingRouletteCoupons;
+}
