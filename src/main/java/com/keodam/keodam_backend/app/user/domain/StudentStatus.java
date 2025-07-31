c371a072785b542d478937ceac9017d3892762de
jihoo2002 <yolohoo@naver.com>
2025-07-31 15:37:25 +0900

    Refactor(#53) : StudentStatus 열거형 클래스 추가

---
+++ src/main/java/com/keodam/keodam_backend/app/user/domain/StudentStatus.java
@@ -0,0 +1,10 @@
+package com.keodam.keodam_backend.app.user.domain;
+
+public enum StudentStatus {
+    HIGH_SCHOOL_GRADUATE,
+    UNIVERSITY_STUDENT,
+    UNIVERSITY_GRADUATE_2_3,
+    UNIVERSITY_GRADUATE_4,
+    JOB_SEEKER,
+    EMPLOYEE;
+}
