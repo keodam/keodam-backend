af4fcef99dffb5d1a8bdef32adb05ea2a7437a13
seunghyun0506 <jno444@kumoh.ac.kr>
2025-08-03 01:35:44 +0900

    Refactor(#57): 밀터 -> 서비스 레이어 ~~~~

--- src/main/java/com/keodam/keodam_backend/oidc/controller/AuthController.java
+++ src/main/java/com/keodam/keodam_backend/oidc/controller/AuthController.java
@@ -1,18 +1,34 @@
 package com.keodam.keodam_backend.oidc.controller;
 
-import com.keodam.keodam_backend.global.security.oidc.IdTokenRequest;
+import com.keodam.keodam_backend.global.ApiResponse;
+import com.keodam.keodam_backend.global.security.JwtService;
+import com.keodam.keodam_backend.oidc.dto.IdTokenRequest;
+import com.keodam.keodam_backend.oidc.dto.IdTokenResponse;
+import com.keodam.keodam_backend.oidc.service.IdTokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.util.Pair;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @Tag(name = "User OIDC Login", description = "OIDC Login API")
 public class AuthController {
 
+    private final IdTokenService idTokenService;
+
     @PostMapping("/api/auth/login")
     @Operation(summary = "소셜 로그인", description = "OIDC 전송하여 로그인하는 소셜 로그인 API")
-    public void login(@RequestBody IdTokenRequest idTokenRequest) {
+    public ResponseEntity<IdTokenResponse> login(@RequestBody IdTokenRequest idTokenRequest) {
+
+        Pair<IdTokenResponse, String> res = idTokenService.loadUserByOidcIdToken(idTokenRequest.provider(), idTokenRequest.idToken());
+
+        return ResponseEntity.ok()
+                .header("Authorization", res.getSecond())
+                .body(res.getFirst());
     }
 }
